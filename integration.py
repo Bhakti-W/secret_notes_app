3d919e003444f004ae8396040884e07debffb4ee
Bhakti Wagh <bhaktii.wagh@gmail.com>
2026-01-12 12:17:07 +0530

    Enhance layout management and reload notes
    Added grid configuration for better layout management and called reload_notes method on notes display.

--- integration.py
+++ integration.py
@@ -5,6 +5,9 @@ from notes_app import NotesFrame
 root = tk.Tk()
 root.title("Calculator")
 root.geometry("500x500")
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(0, weight=1)
+
 
 def show_calc():
     notes.pack_forget()
@@ -13,10 +16,11 @@ def show_calc():
 def show_notes():
     calc.pack_forget()
     notes.pack(fill="both", expand=True)
-
+    notes.reload_notes()
 calc = CalculatorFrame(root, show_notes)
 notes = NotesFrame(root, show_calc,root)
 
 calc.pack(fill="both", expand=True)
 
 root.mainloop()
+
